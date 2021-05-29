10152f7874dc5e3a530002b8d8f140637e464a13
LeVanThao1 <46316006+LeVanThao1@users.noreply.github.com>
2021-05-30 03:18:48 +0700

    update hook

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,28 +1,29 @@
 import { useSubscription } from '@apollo/client';
 import { BOOK_ADMIN_NOTIFICATION } from 'query/notification';
 import React, { useContext, useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useHistory, useLocation } from 'react-router-dom';
 import { message } from 'antd';
 
 const Notification = (props) => {
-  const location = useLocation();
-  const { data } = useSubscription(BOOK_ADMIN_NOTIFICATION, {
-    variables: {
-      userId: sessionStorage.getItem('userId'),
-    },
-  });
-  useEffect(() => {
-    if (data) {
-      if (location.pathname === '/admin/manage-notification') {
-        window.location.reload();
-      } else {
-        message.info(data.receiveNotificationBookAdmin.description);
-      }
-    }
-    return () => {
-      // cleanup;
-    };
-  }, [data]);
-  return <React.Fragment>{props.children}</React.Fragment>;
+    const history = useHistory();
+    const location = useLocation();
+    const { data } = useSubscription(BOOK_ADMIN_NOTIFICATION, {
+        variables: {
+            userId: sessionStorage.getItem('userId'),
+        },
+    });
+    useEffect(() => {
+        if (data) {
+            if (location.pathname === '/admin/manage-notification') {
+                history.go(0);
+            } else {
+                message.info(data.receiveNotificationBookAdmin.description);
+            }
+        }
+        return () => {
+            // cleanup;
+        };
+    }, [data]);
+    return <React.Fragment>{props.children}</React.Fragment>;
 };
 export default Notification;
